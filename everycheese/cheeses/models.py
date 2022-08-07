02d1aacc45b5a7746b16fa16ecd1314fdb12ec82
web3diplomat <web3diplomat@yahoo.com>
2022-08-07 16:47:18 -0400

    some message

---
+++ everycheese/cheeses/models.py
@@ -0,0 +1,24 @@
+from enum import unique
+from msilib import _Unspecified
+from django.db import models
+
+from autoslug import AutoSlugField
+
+from model_utils.models import TimeStampedModel
+
+class Cheese(TimeStampedModel):
+    
+    class Firmness(models.TextChoices):
+        UNSPECIFIED = "unspecified", "Unspecified"
+        SOFT = "soft", "Soft"
+        SEMI_SOFT = "semi-soft", "Semi-Soft"
+        SEMI_HARD = "semi-hard", "Semi-Hard"
+        HARD = "hard", "Hard"
+
+    firmness = models.CharField("Firmness", max_length=20,
+        choices=Firmness.choices, default=Firmness.UNSPECIFIED)
+
+    name = models.CharField("Name of Cheese", max_length=225)
+    slug = AutoSlugField("Cheese Address",
+        unique=True, always_update=False, populate_from="name")
+    description = models.TextField("Description", blank=True)
