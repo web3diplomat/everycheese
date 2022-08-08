85cd163072fa5941cb79acbbadf78b3fd8c268fb
web3diplomat <web3diplomat@yahoo.com>
2022-08-08 02:05:14 -0400

    some words

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -1,21 +1,29 @@
 from django.template.defaultfilters import slugify
 
+import pytest
 import factory
 import factory.fuzzy
 
 from ..models import Cheese
+from everycheese.users.tests.factories import UserFactory
+
 
 class CheeseFactory(factory.django.DjangoModelFactory):
-
     name = factory.fuzzy.FuzzyText()
-    slug = factory.LazyAttribute(
-        lambda obj: slugify(obj.name))
-    description = factory.Faker(
-        'paragraph', nb_sentences=3,
-        variable_nb_sentences=True
-)
-    firmness = factory.fuzzy.FuzzyChoice(
-        [x[0] for x in Cheese.Firmness.choices]
-)
-class Meta:
-    model = Cheese
+    slug = factory.LazyAttribute(lambda obj: slugify(obj.name))
+
+    description = factory.Faker("paragraph", nb_sentences=3, variable_nb_sentences=True)
+
+    firmness = factory.fuzzy.FuzzyChoice([x[0] for x in Cheese.Firmness.choices])
+
+    country_of_origin = factory.Faker("country_code")
+
+    creator = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = Cheese
+
+
+@pytest.fixture
+def cheese():
+    return CheeseFactory()
