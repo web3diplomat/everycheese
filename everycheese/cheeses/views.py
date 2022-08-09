d1b4a4bf46729fd3f649206f36485a2ccc3a6e3c
web3diplomat <103293910+web3diplomat@users.noreply.github.com>
2022-08-09 00:32:28 -0400

    Revert "Update views.py"
    This reverts commit 83f1982b8a4314814d1f67dc9b096c18821df54f.

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,39 +1,9 @@
-
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView
 
 from .models import Cheese
-
 
 class CheeseListView(ListView):
     model = Cheese
 
-
 class CheeseDetailView(DetailView):
     model = Cheese
-
-
-class CheeseCreateView(LoginRequiredMixin, CreateView):
-    model = Cheese
-    fields = [
-        "name",
-        "description",
-        "firmness",
-        "country_of_origin",
-    ]
-    action = "Add"
-
-    def form_valid(self, form):
-        form.instance.creator = self.request.user
-        return super().form_valid(form)
-
-
-class CheeseUpdateView(LoginRequiredMixin, UpdateView):
-    model = Cheese
-    fields = [
-        "name",
-        "description",
-        "firmness",
-        "country_of_origin",
-    ]
-    action = "Update"
