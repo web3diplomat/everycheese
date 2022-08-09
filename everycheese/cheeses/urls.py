eed0851da409b1c190b2270018b78e942646d77e
web3diplomat <103293910+web3diplomat@users.noreply.github.com>
2022-08-09 00:32:21 -0400

    Revert "Update urls.py"
    This reverts commit df02ad61f596cf381a69776d167d967097ead6fe.

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -1,14 +1,23 @@
+# everycheese/cheeses/urls.py
 from django.urls import path
 from . import views
 
 
+
 app_name = "cheeses"
-
 urlpatterns = [
-    path(route="", view=views.CheeseListView.as_view(), name="list"),
-    path(route="add/", view=views.CheeseCreateView.as_view(), name="add"),
+    path(
+        
+        route='',
+        view=views.CheeseListView.as_view(),
+        name='list'
+    ),
 
-    path(route="<slug:slug>/update/", view=views.CheeseUpdateView.as_view(), name="update"),
+    path(
 
-    path(route="<slug:slug>/", view=views.CheeseDetailView.as_view(), name="detail"),
+        route='<slug:slug>/',
+        view=views.CheeseDetailView.as_view(),
+        name='detail'
+    ),
 ]
+
