cd1ff1bd4cfcd84518423fafd114b034312d1de6
web3diplomat <103293910+web3diplomat@users.noreply.github.com>
2022-08-09 01:50:11 -0400

    Add a cheese factory

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -1,29 +1,30 @@
 from django.template.defaultfilters import slugify
 
-import pytest
+
 import factory
 import factory.fuzzy
 
+
 from ..models import Cheese
-from everycheese.users.tests.factories import UserFactory
+
 
 
 class CheeseFactory(factory.django.DjangoModelFactory):
+
     name = factory.fuzzy.FuzzyText()
-    slug = factory.LazyAttribute(lambda obj: slugify(obj.name))
+    
+    slug = factory.LazyAttribute(
+        lambda obj: slugify(obj.name))
+    description = factory.Faker(
+        'paragraph', nb_sentences=3,
 
-    description = factory.Faker("paragraph", nb_sentences=3, variable_nb_sentences=True)
+        variable_nb_sentences=True
 
-    firmness = factory.fuzzy.FuzzyChoice([x[0] for x in Cheese.Firmness.choices])
+    )
+    firmness = factory.fuzzy.FuzzyChoice(
+        [x[0] for x in Cheese.Firmness.choices]
+    )
 
-    country_of_origin = factory.Faker("country_code")
-
-    creator = factory.SubFactory(UserFactory)
 
     class Meta:
-        model = Cheese
-
-
-@pytest.fixture
-def cheese():
-    return CheeseFactory()
+        model = Cheese    
